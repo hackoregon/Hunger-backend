75df4b64c7c0a03e1b0086533fe2b54cf9c442d7
Aaron DeVore <aaron.devore@gmail.com>
2016-05-09 20:44:17 -0700

    Add apiv1 application to backend

--- hunger_backend/hunger_backend/urls.py
+++ hunger_backend/hunger_backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from apiv1 import views as apiv1_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^v1/', apiv1_views),
 ]
